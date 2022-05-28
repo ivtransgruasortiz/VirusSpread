52022dc47b3da91fb08154a3ef292e65a050c145
Iván Ortiz Martín <ivan@transgruasortiz.com>
2022-05-28 20:50:57 +0200

    feat: first simulation movement v1.0

--- virspread/person.py
+++ virspread/person.py
@@ -8,11 +8,15 @@ import numpy as np
 import time
 
 # Constants
-min, max = -15, 15
+min, max = -10, 20
 pasos = 200
-num_pers = 200
+num_pers = 50
 pause = 0.01
-
+value_height = 10
+value_width = 10
+sizemarker = 80
+# func_color = [random.choice(['r', 'b', 'g']) for x in dict_persons]
+func_color = 'r'
 
 class Person:
     # Initial initialization method
@@ -23,27 +27,22 @@ class Person:
             pos_ini = [0, 0, 0]
             pos = list(map(lambda x: random.randint(x, self.dist), pos_ini))
         self.pos = pos
-        print('POS_INI')
-        print(self.pos)
+        # print('POS_INI')
+        # print(self.pos)
 
     # Returns the position of a individual person
     def pos_new(self, last_pos=None):
         delta_pos = list(map(lambda x: random.randint(-1, 1) * self.paso, self.pos))
-        print('DELTA')
-        print(delta_pos)
+        # print('DELTA')
+        # print(delta_pos)
         if last_pos is None:
             pos_paso = [sum(x) for x in zip(self.pos, delta_pos)]
         else:
             pos_paso = [sum(x) for x in zip(last_pos, delta_pos)]
-        print('POS_PASO')
-        print(pos_paso)
-        # # fronteras_circulares
-        # px = px if ((px >= 0) & (px < self.dist)) else abs(abs(px) - self.dist)
-        # py = py if ((py >= 0) & (py < self.dist)) else abs(abs(py) - self.dist)
-        # pz = pz if ((pz >= 0) & (pz < self.dist)) else abs(abs(pz) - self.dist)
-        #
-        # print('pos')
-        # position = [px, py, pz]
+        # print('POS_PASO')
+        # print(pos_paso)
+        # Circular edges
+        pos_paso = [x if ((x >= 0) & (x < self.dist)) else abs(abs(x) - self.dist) for x in pos_paso]
         return delta_pos, pos_paso
 
 
@@ -53,28 +52,30 @@ dict_persons = [{"pos_ini": x.pos,
                  "last_pos": [0, 0, 0],
                  "pos_new": x.pos_new()[1],
                  } for x in list_persons]
-len(list_persons)
 
 # Evolution
 fig = plt.figure()
+fig.set_figheight(value_height)
+fig.set_figwidth(value_width)
 ax = plt.axes(projection='3d')
 
 for i in range(pasos):
-    # list_persons = [Person(pos=x.pos_paso()) for x in list_persons]
     dict_persons = [
         {"pos_ini": x.pos,
          "last_pos": y['pos_new'][1],
          "pos_new": x.pos_new(y['pos_new'])[1],
          }
         for x, y in zip(list_persons, dict_persons)]
-
-    # ax = fig.add_subplot(111, projection='3d')
+    # graphs
     ax.clear()
     ax.set(xlim=(min, max), ylim=(min, max), zlim=(min, max))
     ax.scatter(np.array([x['pos_new'][0] for x in dict_persons]),
                np.array([x['pos_new'][1] for x in dict_persons]),
                np.array([x['pos_new'][2] for x in dict_persons]),
-               zdir='z', s=20, c='r', depthshade=False)
+               zdir='z', s=sizemarker, c=func_color,
+               depthshade=False,
+               marker="H",
+               )
     plt.pause(pause)
 
 
