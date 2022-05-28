6acb12ddb8551785f58928dbecf7c1bebfec2da2
Iván Ortiz Martín <ivan@transgruasortiz.com>
2022-05-28 18:56:31 +0200

    feat: first simulation movement

--- virspread/person.py
+++ virspread/person.py
@@ -2,51 +2,98 @@
 # The class that defines people
 
 import random
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import time
+
+# Constants
+min, max = -15, 15
+pasos = 200
+num_pers = 200
+pause = 0.01
 
 
 class Person:
     # Initial initialization method
-    def __init__(self, pos=[0, 0, 0], dist=10, paso=1, initio=True):
-        self.pos = pos
+    def __init__(self, pos=None, dist=10, paso=1):
         self.paso = paso
-        self.initio = initio
         self.dist = dist
-        self.pos_x = random.randint(self.pos[0], self.dist)
-        self.pos_y = random.randint(self.pos[1], self.dist)
-        self.pos_z = random.randint(self.pos[2], self.dist)
+        if pos is None:
+            pos_ini = [0, 0, 0]
+            pos = list(map(lambda x: random.randint(x, self.dist), pos_ini))
+        self.pos = pos
+        print('POS_INI')
+        print(self.pos)
 
     # Returns the position of a individual person
-    def pos_paso(self):
-        if self.initio:
-            position = [self.pos_x, self.pos_y, self.pos_z]
+    def pos_new(self, last_pos=None):
+        delta_pos = list(map(lambda x: random.randint(-1, 1) * self.paso, self.pos))
+        print('DELTA')
+        print(delta_pos)
+        if last_pos is None:
+            pos_paso = [sum(x) for x in zip(self.pos, delta_pos)]
         else:
-            rx = random.randint(-1, 1) * self.paso
-            ry = random.randint(-1, 1) * self.paso
-            rz = random.randint(-1, 1) * self.paso
-
-            print(rx, ry, rz)
-
-            px = self.pos[0] + rx
-            py = self.pos[1] + ry
-            pz = self.pos[2] + rz
-
-            # if (px >= 0) and (px < self.dist):
-            #     px = px
-            # else:
-            #     px = abs(abs(px) - self.dist)
-
-            px = px if ((px >= 0) & (px < self.dist)) else abs(abs(px) - self.dist)
-            py = py if ((py >= 0) & (py < self.dist)) else abs(abs(py) - self.dist)
-            pz = pz if ((pz >= 0) & (pz < self.dist)) else abs(abs(pz) - self.dist)
-
-            position = [px, py, pz]
-        return position
+            pos_paso = [sum(x) for x in zip(last_pos, delta_pos)]
+        print('POS_PASO')
+        print(pos_paso)
+        # # fronteras_circulares
+        # px = px if ((px >= 0) & (px < self.dist)) else abs(abs(px) - self.dist)
+        # py = py if ((py >= 0) & (py < self.dist)) else abs(abs(py) - self.dist)
+        # pz = pz if ((pz >= 0) & (pz < self.dist)) else abs(abs(pz) - self.dist)
+        #
+        # print('pos')
+        # position = [px, py, pz]
+        return delta_pos, pos_paso
 
 
-list_persons = [Person() for i in range(3)]
+# Initialization
+list_persons = [Person() for i in range(num_pers)]
+dict_persons = [{"pos_ini": x.pos,
+                 "last_pos": [0, 0, 0],
+                 "pos_new": x.pos_new()[1],
+                 } for x in list_persons]
 len(list_persons)
-print('INIT = ', list_persons[0].pos_paso())
 
-for i in range(10):
-    list_persons = [Person(pos=x.pos_paso(), initio=False) for x in list_persons]
-    print(list_persons[0].pos_paso())
+# Evolution
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+for i in range(pasos):
+    # list_persons = [Person(pos=x.pos_paso()) for x in list_persons]
+    dict_persons = [
+        {"pos_ini": x.pos,
+         "last_pos": y['pos_new'][1],
+         "pos_new": x.pos_new(y['pos_new'])[1],
+         }
+        for x, y in zip(list_persons, dict_persons)]
+
+    # ax = fig.add_subplot(111, projection='3d')
+    ax.clear()
+    ax.set(xlim=(min, max), ylim=(min, max), zlim=(min, max))
+    ax.scatter(np.array([x['pos_new'][0] for x in dict_persons]),
+               np.array([x['pos_new'][1] for x in dict_persons]),
+               np.array([x['pos_new'][2] for x in dict_persons]),
+               zdir='z', s=20, c='r', depthshade=False)
+    plt.pause(pause)
+
+
+
+
+
+
+# PoC OK!!
+a = Person()
+pers_dicc = [
+    {"pos_ini": a.pos,
+     "last_pos": [0, 0, 0],
+     "pos_new": a.pos_new(),
+     }
+]
+pers_dicc = [
+    {"pos_ini": a.pos,
+     "last_pos": pers_dicc[0]['pos_new'][1],
+     "pos_new": a.pos_new(pers_dicc[0]['pos_new'][1]),
+     }
+]
+
