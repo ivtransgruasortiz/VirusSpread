83aa5b4f3539b15f3647754d89e5880808b58c9f
Iván Ortiz Martín <ivan@transgruasortiz.com>
2022-06-13 21:03:09 +0200

    feat: added born and dead

--- virspread/person.py
+++ virspread/person.py
@@ -28,6 +28,8 @@ dist_ini = 20  # limit para generacion espontanea
 frontier = 100  # frontera real movimiento
 paso_mov = 1
 repulse = True
+born_rate = 0.2
+death_rate = 0.8
 
 
 class Person:
@@ -66,6 +68,24 @@ def pisado(person_class, dict_persons_=None, last_position=None):
     return result
 
 
+def born(list_person, dict_person):
+    x = Person()
+    list_person.append(x)
+    dict_person.append({"pos_ini": x.pos,
+                        "last_pos": [0, 0, 0],
+                        "pos_new": pisado(x, dict_persons),
+                        })
+    return list_person, dict_person
+
+
+def death(list_person, dict_person):
+    number = len(list_person)
+    perca = random.randint(0, number - 1)
+    list_person.pop(perca)
+    dict_person.pop(perca)
+    return list_person, dict_person
+
+
 # Initialization
 list_persons = [Person() for i in range(num_pers)]
 dict_persons = []
@@ -86,34 +106,53 @@ fig.set_figheight(value_height)
 fig.set_figwidth(value_width)
 ax = plt.axes(projection='3d')
 
-for i in range(pasos):
-    if not repulse:
-        dict_persons = [
-            {"pos_ini": x.pos,
-             "last_pos": y['pos_new'],
-             "pos_new": x.pos_new(y['pos_new'])[1],
-             }
-            for x, y in zip(list_persons, dict_persons)]
-    else:
-        # Opcion con pisado
-        for x, y in zip(list_persons, dict_persons):
-            # print(x, y)
-            y.update({"last_pos": y['pos_new'],
-                      "pos_new": pisado(x, dict_persons, last_position=y['pos_new'])})
-    # graphs
-    ax.clear()
-    ax.set(xlim=(min, max), ylim=(min, max), zlim=(min, max))
-    ax.scatter(np.array([x['pos_new'][0] for x in dict_persons]),
-               np.array([x['pos_new'][1] for x in dict_persons]),
-               np.array([x['pos_new'][2] for x in dict_persons]),
-               zdir='z', s=sizemarker, c=func_color,
-               depthshade=False,
-               marker="H",
-               )
-    plt.pause(pause)
+while len(list_persons) > 0:
+    for i in range(pasos):
+        if not repulse:
+            dict_persons = [
+                {"pos_ini": x.pos,
+                 "last_pos": y['pos_new'],
+                 "pos_new": x.pos_new(y['pos_new'])[1],
+                 }
+                for x, y in zip(list_persons, dict_persons)]
+        else:
+            # Opcion con pisado
+            for x, y in zip(list_persons, dict_persons):
+                # print(x, y)
+                y.update({"last_pos": y['pos_new'],
+                          "pos_new": pisado(x, dict_persons, last_position=y['pos_new'])})
+        prob_born = random.choices([0, 1], [1 - born_rate, born_rate])[0]
+        prob_death = random.choices([0, 1], [1 - death_rate, death_rate])[0]
+        if prob_born == 1:
+            try:
+                borned = born(list_persons, dict_persons)
+                list_persons = borned[0]
+                dict_persons = borned[1]
+            except Exception as e:
+                print(e, 'all dead')
+                pass
+            print('born', len(dict_persons))
+        if prob_death == 1:
+            try:
+                deaths = death(list_persons, dict_persons)
+                list_persons = deaths[0]
+                dict_persons = deaths[1]
+            except Exception as e:
+                print(e, 'all dead')
+                break
+            print('death', len(dict_persons))
 
-
-
+        # graphs
+        ax.clear()
+        ax.set(xlim=(min, max), ylim=(min, max), zlim=(min, max))
+        ax.scatter(np.array([x['pos_new'][0] for x in dict_persons]),
+                   np.array([x['pos_new'][1] for x in dict_persons]),
+                   np.array([x['pos_new'][2] for x in dict_persons]),
+                   zdir='z', s=sizemarker, c=func_color,
+                   depthshade=False,
+                   marker="H",
+                   )
+        plt.pause(pause)
 
 # PoC OK!!
 a = Person()
